480b656f05ed591262890cf92cfa985e53c3de9b
Rebel <johnhartigan@ya.ru>
2017-11-26 12:21:47 +0200

    Less logs.

--- index.js
+++ index.js
@@ -215,14 +215,6 @@ process.on('uncaughtException', (err) => {
     throw err;
 });
 
-nms.nhs.httpServer.on('request', function (req, res) {
-    console.log('http connection.');
-});
-
-nms.nhs.wsServer.on('connection', function (ws, req) {
-    console.log('ws connection.');
-});
-
 nms.nhs.wsServer.close(function () {
     console.log('ws server stopped.');
 });
